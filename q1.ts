ea1817e22ce8b2089959a5e89450ad377a940acf
Apicha660612162 <apichalerdjintanakid@gmail.com>
2024-07-02 18:07:29 +0700

    gg

--- q1.ts
+++ q1.ts
@@ -1,17 +1,20 @@
-function typeChecker(a, b) {
-  /* Your code here */
-}
-
-//Test cases
-const a1 = "Jack";
-const b1 = "Rose";
-const a2 = 555;
-const b2 = 100;
-const a3 = "Jack";
-const b3 = 100;
-
-console.log(typeChecker(a1, b1));
-console.log(typeChecker(a2, b2));
-console.log(typeChecker(a3, b3));
-
-module.exports = typeChecker;
+function typeChecker(a :any, b : number | string){
+  if (typeof a && typeof b === `string`) return "I LOVE YOU ";
+  if(typeof a !== typeof b ) return " NOT MATCHED";
+  if(typeof a && typeof b === "number") return a + b;
+ }
+ 
+ //Test cases
+ const a1 = "Jack";
+ const b1 = "Rose";
+ const a2 = 555;
+ const b2 = 100;
+ const a3 = "Jack";
+ const b3 = 100;
+ 
+ console.log(typeChecker(a1, b1));
+ console.log(typeChecker(a2, b2));
+ console.log(typeChecker(a3, b3));
+ 
+ module.exports = typeChecker;
+ 
